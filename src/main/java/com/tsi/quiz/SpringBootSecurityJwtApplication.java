7034a7d61666511115d4c2773420075b62ff887a
jhon <lopiccoloottavio@gmail.com>
2021-04-14 21:07:05 +0200

    hh

--- src/main/java/com/tsi/quiz/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/tsi/quiz/SpringBootSecurityJwtApplication.java
@@ -3,9 +3,13 @@ package com.tsi.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class SpringBootSecurityJwtApplication  extends SpringBootServletInitializer{
+@EnableAsync
+
+public class SpringBootSecurityJwtApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
