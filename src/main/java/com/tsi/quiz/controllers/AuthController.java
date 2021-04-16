0ba1570238752e5d06cd5895e30c3ba6932f749b
jhon <lopiccoloottavio@gmail.com>
2021-04-16 15:10:54 +0200

    yyv

--- src/main/java/com/tsi/quiz/controllers/AuthController.java
+++ src/main/java/com/tsi/quiz/controllers/AuthController.java
@@ -95,7 +95,7 @@ public class AuthController {
 		Set<Role> roles = new HashSet<>();
 
 		if (strRoles == null) {
-			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+			Role userRole = roleRepository.findByName(ERole.ROLE_ADMIN)
 					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
 			roles.add(userRole);
 		} else {
