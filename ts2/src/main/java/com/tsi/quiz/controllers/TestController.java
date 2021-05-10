d79bfb092e1ec0e7787e7ebbbb976ca8ccae419c
Ottavio93 <lopiccoloottavio@gmail.com>
2021-05-10 16:45:26 +0200

    yhyh

--- ts2/src/main/java/com/tsi/quiz/controllers/TestController.java
+++ ts2/src/main/java/com/tsi/quiz/controllers/TestController.java
@@ -555,4 +555,26 @@ public class TestController {
 	    quizRepository.deleteById("cu78ypgm");
 	     return new ResponseEntity<>(HttpStatus.CREATED);
 	    }
+	    @PostMapping("allreportsDelete")
+	    public ResponseEntity<Void> allreportsDelete( ) {
+	    
+	    	
+	    	Optional<Quiz> optionalquiz=quizRepository.findById("cu78ypgm");
+	    	Quiz quiz = optionalquiz
+	    			.orElseThrow(() -> new UsernameNotFoundException("No tipoQuiz " +
+							"Found with tipoQuizId : " ));
+	    	System.out.print("nooooooooofgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggooooo");
+	     	List<TipoQuiz> tipiQuiz=   tipoQuizRepository.findByQuiz(quiz);
+	    	
+	     	for (TipoQuiz q:tipiQuiz) {
+	     		TipoQuizDelete tipoQuizDelete=new TipoQuizDelete(q.getTipoQuizId());
+	     		
+	     		tipoquizDelete(tipoQuizDelete);
+	    
+	     	System.out.println("fatto");
+	     	}
+	     
+	    quizRepository.deleteById("cu78ypgm");
+	     return new ResponseEntity<>(HttpStatus.CREATED);
+	    }
 }
